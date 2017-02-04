b258c621ceabc17e6a8f0d5191e749e0670ff1f4
Jack Wright <jackrwright1@gmail.com>
2017-02-04 14:57:31 -0500

    Added a shadow to the card images to help with those images that have no outline.

--- MemoryGame/CardView.swift
+++ MemoryGame/CardView.swift
@@ -16,6 +16,8 @@ class CardView: UIButton {
 		// set myType before super.init is called
 		self.myType = type
 		
+		// These dimensions match the @1x image size,
+		// but the image may be redimensioned by the UIStackView.
 		let width: CGFloat = 74
 		let height: CGFloat = 106
 		
@@ -24,8 +26,16 @@ class CardView: UIButton {
 		
 		// This keeps the aspect ratio of the card image constant
 		self.imageView?.contentMode = .scaleAspectFit
-				
-		// set other operations after super.init, if required
+		
+		// Add a shadow to help with those images that have no outline.
+		// I tried to appoximate the shadow on the provided back button artwork.
+		self.layer.shadowOffset = CGSize.init(width: -2, height: 2)
+		self.layer.shadowOpacity = 0.4;
+		self.layer.shadowRadius = 5.0;
+		self.clipsToBounds = false;
+		self.layer.shouldRasterize = true;
+
+		// start by showing the card back
 		showBack()
 	}
 	
@@ -34,6 +44,7 @@ class CardView: UIButton {
 		fatalError("init(coder:) has not been implemented")
 	}
 	
+	// Could do some animations here
 	func showFace()
 	{
 		if let imageName = CardType.faceImageForType(myType) {
